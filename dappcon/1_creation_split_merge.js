49d00e0acd1fe2000f278a03a1630783c68d1a07
jakub-wojciechowski <jakub@alice.si>
2019-08-24 18:00:20 +0200

    test

--- dappcon/1_creation_split_merge.js
+++ dappcon/1_creation_split_merge.js
@@ -18,12 +18,12 @@ contract('Should fully split and merge', function ([user, oracle]) {
   });
 
   step("1. We should bind the HG library to the contract address", async function () {
-
+    hg = new HG(pms.address);
     hg.should.be.not.null;
   });
 
   step("2. Let's create the first condition", async function () {
-
+    condition = await hg.prepareCondition('First condition', oracle, 2);
     condition.should.be.not.null;
   });
 
@@ -32,8 +32,11 @@ contract('Should fully split and merge', function ([user, oracle]) {
   step("3. Let's create the first split", async function () {
     //Beware of minting the collateral tokens
     //and making an allowance first by approving a desired value of collateral to the pms contract
+    await collateral.mint(user, 100);
+    await collateral.approve(pms.address, 100);
 
     //Remember to pass the collateral amount to the split function
+    let positions = await condition.fullSplit(collateral.address, 100);
 
     positions.length.should.be.equal(2);
 
@@ -49,6 +52,7 @@ contract('Should fully split and merge', function ([user, oracle]) {
 
   step("4. Let's fully merge the positions", async function () {
     //Remember of passing the desired amount of tokens to be merged
+    await condition.mergeAll(collateral.address, 100);
 
     //We can check the balance of collateral held by user and the pms contract
     (await collateral.balanceOf(user)).should.be.bignumber.equal('100');
