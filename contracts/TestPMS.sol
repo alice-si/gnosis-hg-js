194828b152022dc39692b836e6240a9dc3534275
Alan Lu <alanlu1023@gmail.com>
2019-09-16 11:42:14 -0500

    updated to internally use new conditional tokens implementation

--- contracts/TestPMS.sol
+++ contracts/TestPMS.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.5.2;
 Used for test purpose only to be able for automatic binding by truffle
 */
 
-import '@gnosis.pm/hg-contracts/contracts/PredictionMarketSystem.sol';
+import '@gnosis.pm/conditional-tokens-contracts/contracts/ConditionalTokens.sol';
 
-contract TestPMS is PredictionMarketSystem {
+contract TestPMS is ConditionalTokens {
 
 }
